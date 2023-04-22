05d3c2b06d8d036d0f5c8c13c5e5faf4a06faec9
itzgupta <itz.palashgupta@gmail.com>
2023-04-22 23:29:45 +0530

    Login-update

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,10 @@
 import Input from '@/components/Input'
-import React from 'react'
+import React, { useState } from 'react'
 
 const auth = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+
   return (
     <div className='relative h-full w-full bg-[url("/images/loginbanner.png")] bg-no-repeat bg-center bg-fixed'>
         <div className='bg-black w-full h-full lg:bg-opacity-40'>
@@ -15,7 +18,26 @@ const auth = () => {
                         Log in
                     </h2>
                     <div className='flex flex-col gap-4'>
-                        <Input />
+                        <Input 
+                            label='Username'
+                            onChange={(ev: any) => setName (ev.target.value)}
+                            id='name'
+                            value={name}
+                        />
+                        <Input 
+                            label='Email'
+                            onChange={(ev: any) => setEmail (ev.target.value)}
+                            id='email'
+                            type='email'
+                            value={email}
+                        />
+                        <Input 
+                            label='Password'
+                            onChange={(ev: any) => setEmail (ev.target.value)}
+                            id='password'
+                            type='password'
+                            value={email}
+                        />
                     </div>
                 </div>
             </div>
