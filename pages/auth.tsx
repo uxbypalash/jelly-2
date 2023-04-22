ee14d831d69bd179f8a151cd03236497d4b7d501
itzgupta <itz.palashgupta@gmail.com>
2023-04-22 23:46:36 +0530

    Log in/Sign up - Toggle - Update - 1

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,18 @@
 import Input from '@/components/Input'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 const auth = () => {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    // ---- Toggle of login & New account
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    }, [])
+
 
   return (
     <div className='relative h-full w-full bg-[url("/images/loginbanner.png")] bg-no-repeat bg-center bg-fixed'>
@@ -15,15 +24,19 @@ const auth = () => {
             <div className='flex justify-center'>
                 <div className='bg-black bg-opacity-90 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
                     <h2 className='text-white text-4xl mb-8 font-semibold'>
-                        Log in
+                        {variant === 'login' ? 'Sign in' : 'Register'}
                     </h2>
                     <div className='flex flex-col gap-4'>
-                        <Input 
-                            label='Username'
-                            onChange={(ev: any) => setName (ev.target.value)}
-                            id='name'
-                            value={name}
-                        />
+                        {/* ---- Toggle */}
+                        {variant === 'register' && (
+                            <Input 
+                                label='Username'
+                                onChange={(ev: any) => setName (ev.target.value)}
+                                id='name'
+                                value={name}
+                            />
+                        )}
+                        {/* ---- */}
                         <Input 
                             label='Email'
                             onChange={(ev: any) => setEmail (ev.target.value)}
@@ -33,12 +46,23 @@ const auth = () => {
                         />
                         <Input 
                             label='Password'
-                            onChange={(ev: any) => setEmail (ev.target.value)}
+                            onChange={(ev: any) => setPassword (ev.target.value)}
                             id='password'
                             type='password'
-                            value={email}
+                            value={password}
                         />
                     </div>
+                    <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+                        {/* ---- Button Label Change on toggle */}
+                        {variant === 'login' ? 'Login' : 'Sign up'}
+                    </button>
+
+                    <p className='text-neutral-500 mt-12'>
+                        First time using Jelly?
+                        <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
+                            Create an account
+                        </span>
+                    </p>
                 </div>
             </div>
         </div>
