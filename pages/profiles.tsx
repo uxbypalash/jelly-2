99125441ec7b6944cced10358f59e2ca88cdd2e7
itzgupta <itz.palashgupta@gmail.com>
2023-04-24 01:17:33 +0530

    Profiles page - UPDATE

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,5 +1,7 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next'
 import { getSession } from 'next-auth/react'
+import { useRouter } from 'next/router';
 import React from 'react'
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -20,9 +22,29 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const profiles = () => {
+    const router = useRouter();
+    const { data: user } = useCurrentUser();
+
   return (
-    <div>
-        <p className='text-white text-4xl'>Profiles</p>
+    <div className='flex items-center h-full justify-center'>
+        <div className='flex flex-col'>
+            <h1 className='text-3xl font-medium md:text-6xl text-white text-center'>Who is chilling?</h1>
+            <div className='flex items-center justify-center gap-8 mt-10'>
+                <div onClick={() => router.push('/')}>
+
+                    <div className='group flex-row w-44 mx-auto'>
+                        <div className='w-44 h-44 rounded-full flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden'>
+                            <img src="/images/defaultpurple.png" alt="Profile" />
+                        </div>
+
+                        <div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white'>
+                            {user?.name}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
