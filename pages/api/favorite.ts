a4f6b38a0ea001364091ec8421789fd52cfbd159
itzgupta <itz.palashgupta@gmail.com>
2023-04-26 01:04:57 +0530

    update - 8

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -5,6 +5,7 @@ import prismadb from '@/lib/prismadb';
 import serverAuth from "@/lib/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    
     try {
         if (req.method !== 'POST') {
             const { currentUser } = await serverAuth(req);
