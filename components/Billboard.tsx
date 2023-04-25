ca40a55cdee1f45876dceed58ce03ab19ee95d49
itzgupta <itz.palashgupta@gmail.com>
2023-04-25 19:04:59 +0530

    Billboard - update - 2

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,9 +1,17 @@
+import useBillboard from '@/hooks/useBillboard'
 import React from 'react'
 
 const Billboard = () => {
+    const { data } = useBillboard();
+
   return (
-    <div>
-        
+    <div className='relative h-[56.25vw]'>
+        <video className='w-full h-[56.25vw] sm:h-[70%] object-cover brightness-[60%]' autoPlay muted loop poster={data?.thumbnailUrl} src={data?.videoUrl}></video>
+        <div className='absolute top-[30%] md:top-[40%] ml-4 md:ml-16'>
+          <p className='text-white text-1xl md:text-5xl'>
+            {data?.title}
+          </p>
+        </div>
     </div>
   )
 }
--- hooks/useBillboard.ts
+++ hooks/useBillboard.ts
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 import fetcher from '@/lib/fetcher';
 
 const useBillboard = () => {
-    const { data, error, isLoading } = useSWR('/apo/random', fetcher, {
+    const { data, error, isLoading } = useSWR('/api/random', fetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
         revalidateOnReconnect: false,
