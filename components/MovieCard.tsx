ca9906670ee41349cbf952ca87d1d7cc6814ecfe
itzgupta <itz.palashgupta@gmail.com>
2023-04-26 00:50:39 +0530

    list add script added

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -10,7 +10,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
 }) => {
   return (
     <div className='group bg-[#000018] col-span relative h-[12vw]'>
-        <img className='cursor-pointer object-cover transition duration shadow-xl rounded-md gropu-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]' src={data.thumbnailUrl} alt="Thumbnail" />
+        <img className='cursor-pointer object-cover transition duration shadow-xl rounded-md gropu-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full sm:h-[12vw]' src={data.thumbnailUrl} alt="Thumbnail" />
 
         <div className='opacity-0 absolute top-0 transition duration-200 z-50 invisible sm:invisible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[2vw] group-hover:translate-x-[1vw] group-hover:visible group-hover:opacity-100'>
             <img className='cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]' src={data.thumbnailUrl} alt="Thumbnail" />
@@ -35,6 +35,18 @@ const MovieCard: React.FC<MovieCardProps> = ({
                     >
                         <BsFillPlayFill size={35} />
                     </div>
+                </div>
+
+                <p className='text-green-400 font-semibold mt-4'>
+                    New <span className='text-white '>2023</span>
+                </p>
+
+                <div className='flex flex-row mt-4 gap-2 items-center'>
+                    <p className='text-white text-[10px] lg:text-sm'>{data.duration}</p>
+                </div>
+
+                <div className='flex flex-row mt-4 gap-2 items-center'>
+                    <p className='text-white text-[10px] lg:text-sm'>{data.genre}</p>
                 </div>
             </div>
         </div>
