a7ff902bc4c2ad498b4496ac592ed4525d6860a2
itzgupta <itz.palashgupta@gmail.com>
2023-04-26 18:04:44 +0530

    update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -31,7 +31,7 @@ export default NextAuth({
                 },
             },
             async authorize(credentials) {
-                if (!credentials?.email || !credentials ?.password) {
+                if (!credentials?.email || !credentials?.password) {
                     throw new Error('Email and password required');
                 }
 
